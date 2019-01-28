4a9893c19b2cf5007a0f90e7bfab72644918ec2f
gongshuangshuang <47023139+gongshuangshuang@users.noreply.github.com>
2019-01-28 16:38:32 +0800

    Update data_preprocess.py

--- code/data_preprocess.py
+++ code/data_preprocess.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import train_test_split
 
 # -------------数据处理------------
 def data_process():
-    with open('/data/movie_lines.txt', 'rb') as fin, \
-            open('preprocessed_movie_lines.txt', 'w', encoding='utf-8') as fout:
+    with open('../data/movie_lines.txt', 'rb') as fin, \
+            open('../data/preprocessed_movie_lines.txt', 'w', encoding='utf-8') as fout:
         for lines in fin.readlines():
             line = str(lines).strip().split('+++$+++')[-1].split('\\n')[0].replace('\\', '')
             s = line.strip().lower()
@@ -13,10 +13,10 @@ def data_process():
 
 # -------------数据切分------------
 def data_split():
-    with open('preprocessed_movie_lines.txt', 'r', encoding='utf-8') as fin, \
-        open('movie_dialog_train.txt', 'w', encoding='utf-8') as f_train, \
-        open('movie_dialog_val.txt', 'w', encoding='utf-8') as f_val, \
-        open('movie_dialog_test.txt', 'w', encoding='utf-8') as f_test:
+    with open('../data/preprocessed_movie_lines.txt', 'r', encoding='utf-8') as fin, \
+        open('../data/movie_dialog_train.txt', 'w', encoding='utf-8') as f_train, \
+        open('../data/movie_dialog_val.txt', 'w', encoding='utf-8') as f_val, \
+        open('../data/movie_dialog_test.txt', 'w', encoding='utf-8') as f_test:
         data = fin.readlines()
         train_data, valtest_data = train_test_split(data, test_size=0.2, random_state=0)
         val_data, test_data = train_test_split(valtest_data, test_size=0.5, random_state=0)
